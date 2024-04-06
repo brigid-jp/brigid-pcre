5a777b4da4b649bfd325fda1aa35f2494e5dd978
dev <dev@brigid.jp>
2024-04-06 19:50:55 +0900

    ビルドスクリプトを分割して変数の継承を整理。

--- Makefile
+++ Makefile
@@ -1,7 +1,12 @@
 export ROCK_CFLAGS ROCK_LIBFLAG ROCK_LUA_INCDIR ROCK_LIBDIR
 
 all:
-	make -C build -j 8
+	make -C build -f pcre2.mk
+	make -C build
+
+clean:
+	make -C build -f pcre2.mk clean
+	make -C build clean
 
 install:
 	@echo noop
---
+++ build/pcre2.mk
@@ -0,0 +1,26 @@
+pcre2_version = 10.43
+pcre2_package = pcre2-$(pcre2_version)
+pcre2_archive = $(pcre2_package).tar.gz
+pcre2_url = https://github.com/PCRE2Project/pcre2/releases/download/$(pcre2_package)/$(pcre2_archive)
+pcre2_library = pcre2/lib/libpcre2-8.a
+
+export CFLAGS
+CFLAGS += $(ROCK_CFLAGS)
+# CPPFLAGS += -D
+
+all: $(pcre2_library)
+
+clean:
+	rm -f -r pcre2 $(pcre2_package)
+
+$(pcre2_archive):
+	curl -fLO $(pcre2_url)
+
+$(pcre2_package): $(pcre2_archive)
+	gunzip -cd $(pcre2_archive) | tar xf -
+
+$(pcre2_library): $(pcre2_package)
+	@echo $(CPPFLAGS)
+	@echo $(LDFLAGS)
+	(cd $(pcre2_package) && ./configure --prefix="`pwd`/../pcre2" --disable-shared --disable-jit)
+	make V=1 -C $(pcre2_package) -j 8 install-nodist_includeHEADERS install-libLTLIBRARIES
