bb705f4200a5722e011d66c4addf24276d2798c2
dev <dev@brigid.jp>
2024-04-06 21:18:40 +0900

    依存関係の解決を精緻化。

--- build/pcre2.mk
+++ build/pcre2.mk
@@ -2,7 +2,6 @@ pcre2_version = 10.43
 pcre2_package = pcre2-$(pcre2_version)
 pcre2_archive = $(pcre2_package).tar.gz
 pcre2_url = https://github.com/PCRE2Project/pcre2/releases/download/$(pcre2_package)/$(pcre2_archive)
-pcre2_extracted = $(pcre2_package)/.extracted
 pcre2_library = pcre2/lib/libpcre2-8.a
 
 export CFLAGS CPPFLAGS
@@ -12,15 +11,16 @@ CPPFLAGS += -DPCRE2_EXP_DECL= -DPCRE2_EXP_DEFN=
 all: $(pcre2_library)
 
 clean:
-	rm -f -r pcre2 $(pcre2_package)
+	rm -f -r $(pcre2_package) $(pcre2_package).ready pcre
 
-$(pcre2_archive):
+$(pcre2_package).fetch:
 	curl -fLO $(pcre2_url)
+	touch $(pcre2_package).fetch
 
-$(pcre2_extracted): $(pcre2_archive)
+$(pcre2_package).ready: $(pcre2_package).fetch
 	gunzip -cd $(pcre2_archive) | tar xf -
-	touch $(pcre2_extracted)
+	touch $(pcre2_package).ready
 
-$(pcre2_library): $(pcre2_extracted)
+$(pcre2_library): $(pcre2_package).ready
 	(cd $(pcre2_package) && ./configure --prefix="`pwd`/../pcre2" --disable-shared --disable-jit)
 	make -C $(pcre2_package) -j 8 install-nodist_includeHEADERS install-libLTLIBRARIES
