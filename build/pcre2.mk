8afeeae7f797fa08a7308d37d4524fad938f442e
dev <dev@brigid.jp>
2024-04-06 20:45:29 +0900

    tarballからの展開時に依存関係が正しく解決されるように修正。

--- build/pcre2.mk
+++ build/pcre2.mk
@@ -2,6 +2,7 @@ pcre2_version = 10.43
 pcre2_package = pcre2-$(pcre2_version)
 pcre2_archive = $(pcre2_package).tar.gz
 pcre2_url = https://github.com/PCRE2Project/pcre2/releases/download/$(pcre2_package)/$(pcre2_archive)
+pcre2_extracted = $(pcre2_package)/.extracted
 pcre2_library = pcre2/lib/libpcre2-8.a
 
 export CFLAGS CPPFLAGS
@@ -16,9 +17,10 @@ clean:
 $(pcre2_archive):
 	curl -fLO $(pcre2_url)
 
-$(pcre2_package): $(pcre2_archive)
+$(pcre2_extracted): $(pcre2_archive)
 	gunzip -cd $(pcre2_archive) | tar xf -
+	touch $(pcre2_extracted)
 
-$(pcre2_library): $(pcre2_package)
+$(pcre2_library): $(pcre2_extracted)
 	(cd $(pcre2_package) && ./configure --prefix="`pwd`/../pcre2" --disable-shared --disable-jit)
 	make -C $(pcre2_package) -j 8 install-nodist_includeHEADERS install-libLTLIBRARIES
