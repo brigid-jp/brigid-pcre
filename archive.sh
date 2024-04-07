58d3013440a4e46c18ff81c2d26541f7d7adc95a
dev <dev@brigid.jp>
2024-04-07 15:59:42 +0900

    ビルドスクリプトを更新。

--- archive.sh
+++ archive.sh
@@ -6,4 +6,6 @@ echo "$version"
 prefix=brigid-pcre2-$version
 
 git archive --prefix "$prefix/" HEAD | (cd build && tar xf -)
-make -C build/$prefix/build -f pcre2.mk archive
+make -C "build/$prefix/build" -f pcre2.mk archive
+(cd build && zip -r9q "$prefix.zip" "$prefix")
+rm -f -r "build/$prefix"
