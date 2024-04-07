3dc4b3060f951c25acf92e10cbb5efacf6569fa3
dev <dev@brigid.jp>
2024-04-07 15:33:28 +0900

    ovectorの取得。

--- match_data.cpp
+++ match_data.cpp
@@ -16,7 +16,17 @@ namespace brigid {
       self_t::newuserdata(L, std::move(self));
     }
 
-    void impl_get_bynumber(lua_State* L) {
+    void impl_get_by_name(lua_State* L) {
+      auto self = self_t::checkudata(L, 1);
+      auto name = checkstring(L, 2);
+      std::uint8_t* data = nullptr;
+      std::size_t size = 0;
+      check(pcre2_substring_get_byname(self, name.data_u8(), &data, &size));
+      lua_pushlstring(L, reinterpret_cast<const char*>(data), size);
+      pcre2_substring_free(data);
+    }
+
+    void impl_get_by_number(lua_State* L) {
       auto self = self_t::checkudata(L, 1);
       auto number = luaL_checkinteger(L, 2);
       std::uint8_t* data = nullptr;
@@ -26,14 +36,27 @@ namespace brigid {
       pcre2_substring_free(data);
     }
 
-    void impl_get_byname(lua_State* L) {
+    void impl_get(lua_State* L) {
+      if (lua_type(L, 2) == LUA_TSTRING) {
+        impl_get_by_name(L);
+      } else {
+        impl_get_by_number(L);
+      }
+    }
+
+    void impl_get_ovector(lua_State* L) {
       auto self = self_t::checkudata(L, 1);
-      auto name = checkstring(L, 2);
-      std::uint8_t* data = nullptr;
-      std::size_t size = 0;
-      check(pcre2_substring_get_byname(self, name.data_u8(), &data, &size));
-      lua_pushlstring(L, reinterpret_cast<const char*>(data), size);
-      pcre2_substring_free(data);
+      auto data = pcre2_get_ovector_pointer(self);
+      auto size = pcre2_get_ovector_count(self);
+      lua_newtable(L);
+      for (std::uint32_t i = 0; i < size; ++i) {
+        lua_newtable(L);
+        lua_pushinteger(L, data[i * 2] + 1);
+        lua_rawseti(L, -2, 1);
+        lua_pushinteger(L, data[i * 2 + 1]);
+        lua_rawseti(L, -2, 2);
+        lua_rawseti(L, -2, i);
+      }
     }
   }
 
@@ -50,8 +73,10 @@ namespace brigid {
       setfield(L, -1, "__call", function<impl_construct>());
       lua_setmetatable(L, -2);
 
-      setfield(L, -1, "get_bynumber", function<impl_get_bynumber>());
-      setfield(L, -1, "get_byname", function<impl_get_byname>());
+      setfield(L, -1, "get_by_name", function<impl_get_by_name>());
+      setfield(L, -1, "get_by_number", function<impl_get_by_number>());
+      setfield(L, -1, "get", function<impl_get>());
+      setfield(L, -1, "get_ovector", function<impl_get_ovector>());
     }
     lua_setfield(L, -2, "match_data");
   }
--- test/test_code.lua
+++ test/test_code.lua
@@ -18,12 +18,38 @@ assert(regex:info(pcre2.INFO_NAMECOUNT) == 3)
 
 local result, match = regex:match "abc2024/04/01def"
 assert(result == 4)
-assert(match:get_bynumber(0) == "2024/04/01")
-assert(match:get_bynumber(1) == "2024")
-assert(match:get_bynumber(2) == "04")
-assert(match:get_bynumber(3) == "01")
-test.assume_fail(function () return match:get_bynumber(4) end)
-assert(match:get_byname "year" == "2024")
-assert(match:get_byname "month" == "04")
-assert(match:get_byname "day" == "01")
-test.assume_fail(function () return match:get_byname "" end)
+assert(match:get_by_number(0) == "2024/04/01")
+assert(match:get_by_number(1) == "2024")
+assert(match:get_by_number(2) == "04")
+assert(match:get_by_number(3) == "01")
+test.assume_fail(function () return match:get_by_number(4) end)
+assert(match:get_by_name "year" == "2024")
+assert(match:get_by_name "month" == "04")
+assert(match:get_by_name "day" == "01")
+test.assume_fail(function () return match:get_by_name "" end)
+
+assert(match:get(1) == "2024")
+assert(match:get(2) == "04")
+assert(match:get(3) == "01")
+assert(match:get "year" == "2024")
+assert(match:get "month" == "04")
+assert(match:get "day" == "01")
+
+assert(match:get(1) == "2024")
+assert(match:get "year" == "2024")
+
+local ov = match:get_ovector()
+if test.debug then
+  print(0, ov[0][1], ov[0][2])
+  for i, o in ipairs(ov) do
+    print(i, o[1], o[2])
+  end
+end
+assert(ov[0][1] == 4)
+assert(ov[0][2] == 13)
+assert(ov[1][1] == 4)
+assert(ov[1][2] == 7)
+assert(ov[2][1] == 9)
+assert(ov[2][2] == 10)
+assert(ov[3][1] == 12)
+assert(ov[3][2] == 13)
